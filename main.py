940d5eaae3bd908274d3a4d30cd4676456cb3a63
Anton Golubchikov <anton1programmist@gmail.com>
2025-01-22 20:26:07 +0700

    added basic main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from settings.logger import get_logger
+from settings.constant import data
+from signals import Signals
+
+logger = get_logger()
+
+
+class Neuro:
+    pass
+
+
+if __name__ == '__main__':
+    neuro = Neuro()
